398c66643eb7d93082b56b90de3036d61e10b8e3
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2022-12-07 16:52:33 +0100

    build(project): create user controller to handle api and calling the right service

---
+++ src/main/java/com/brief/arbre_des_competences/users/UserController.java
@@ -0,0 +1,23 @@
+package com.brief.arbre_des_competences.users;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RestController
+@RequestMapping("/api/user")
+@CrossOrigin(origins = "http://localhost:4200")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/login")
+    public String loginController(@RequestBody String email, @RequestBody String password) {
+        System.out.println(email);
+        return userService.loginService(email, password);
+    }
+
+
+
+}
