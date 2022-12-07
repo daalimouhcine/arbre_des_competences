9404ea70a6006fd61eb2b767f12b6a4d7171ed41
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2022-12-07 16:50:39 +0100

    build(project): create utils for sending mails and out puting message with colors and for hashing password and check the password with the hash

---
+++ src/main/java/com/brief/arbre_des_competences/utils/HashPassword.java
@@ -0,0 +1,14 @@
+package com.brief.arbre_des_competences.utils;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class HashPassword {
+    public static String hash(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public static boolean check(String password, String hashedPassword) {
+        return BCrypt.checkpw(password, hashedPassword);
+    }
+
+}
