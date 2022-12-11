14753398cd5097f6bf25395f1c04a6411946bc15
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2022-12-11 22:42:06 +0100

    build(project): modify the user entity

--- src/main/java/com/brief/arbre_des_competences/users/UserEntity.java
+++ src/main/java/com/brief/arbre_des_competences/users/UserEntity.java
@@ -8,7 +8,7 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
-    private long id;
+    private Long id;
 
     @Column
     @Basic
@@ -42,11 +42,11 @@ public class UserEntity {
     @Basic
     private String promotion;
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
