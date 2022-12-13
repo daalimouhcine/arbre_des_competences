d4cf7fe90c144e5e0d9d729937309ec62a2b4152
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2022-12-13 23:27:13 +0100

    build(project):  create ReferenceSkillController

---
+++ src/main/java/com/brief/arbre_des_competences/skillsReference/ReferenceSkillController.java
@@ -0,0 +1,23 @@
+package com.brief.arbre_des_competences.skillsReference;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+@RequestMapping("/api/skill")
+@CrossOrigin(origins = "*")
+public class ReferenceSkillController {
+    @Autowired
+    private ReferenceSkillService referenceSkillService;
+
+    @GetMapping("/referenceSkills")
+    public String getAllReferenceSkills() {
+        return referenceSkillService.getAllReferenceSkills();
+    }
+
+}
