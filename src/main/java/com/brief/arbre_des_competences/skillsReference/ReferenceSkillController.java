234a83161b9df92a67ea80db047e82448abd8525
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2022-12-16 22:46:02 +0100

    build(project):  modify file

--- src/main/java/com/brief/arbre_des_competences/skillsReference/ReferenceSkillController.java
+++ src/main/java/com/brief/arbre_des_competences/skillsReference/ReferenceSkillController.java
@@ -1,5 +1,6 @@
 package com.brief.arbre_des_competences.skillsReference;
 
+import com.brief.arbre_des_competences.utils.Sout;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
