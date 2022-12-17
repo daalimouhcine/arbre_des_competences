fabbc6c26cf7fe2bdf817341e3ed3681be151c4f
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2022-12-18 00:00:15 +0100

    build(project): modify repository

--- src/main/java/com/brief/arbre_des_competences/skillsReference/ReferenceSkillRepository.java
+++ src/main/java/com/brief/arbre_des_competences/skillsReference/ReferenceSkillRepository.java
@@ -1,5 +1,6 @@
 package com.brief.arbre_des_competences.skillsReference;
 
+import com.brief.arbre_des_competences.competence.CompetenceEntity;
 import com.brief.arbre_des_competences.config.EntityManagerConfig;
 import com.brief.arbre_des_competences.utils.Sout;
 import com.google.gson.Gson;
@@ -12,14 +13,19 @@ import java.util.List;
 @Repository
 public class ReferenceSkillRepository {
 
-    public String allReferenceSkills() {
+    public List<ReferenceSkillEntity> allReferenceSkills() {
             EntityManager em = EntityManagerConfig.getInstance().getEm();
         try {
             em.getTransaction().begin();
             Query query = em.createQuery("SELECT R FROM ReferenceSkillEntity R");
             List<ReferenceSkillEntity> referenceSkillsList = query.getResultList();
             em.getTransaction().commit();
-            return new Gson().toJson(referenceSkillsList);
+            for (ReferenceSkillEntity referenceSkill : referenceSkillsList) {
+                for (CompetenceEntity competence : referenceSkill.getCompetences()) {
+                    competence.setReferenceSkill(null);
+                }
+            }
+            return referenceSkillsList;
         } catch (Exception e) {
             Sout.sout("red", e.getMessage());
             return null;
